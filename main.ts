4fe3e4f1fadebda35590ac4ce669f8d347077f43
MrZebarth <kris.zebarth@ocsb.ca>
2022-10-11 09:14:58 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
+let num1 = 0
+let num2 = 0
+let result = 0
+function GetRandom () {
+    return randint(0, 10)
+}
+input.onButtonPressed(Button.A, function () {
+    Roll_Dice()
+})
+// Pick two random numbers, add them together, send them back.
+function Roll_Dice () {
+    num1 = GetRandom()
+    num2 = GetRandom()
+    result = num1 + num2
+    basic.showNumber(result)
+}
 basic.forever(function () {
 	
 })
